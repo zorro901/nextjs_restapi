fa5cdf1bea1f295cba13dc2ab58790aecbbe932d
zorro901 <zoro927@hotmail.com>
2021-08-12 19:16:56 +0900

    fixed

--- rest_api/wsgi.py
+++ rest_api/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-form dj_static import Cling
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api.settings')
